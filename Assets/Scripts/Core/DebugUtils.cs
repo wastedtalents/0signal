3932da3106fe46a79d0b82ba724c5225409c518b
Jarek Wawro <jarek.wawro@gmail.com>
2014-04-23 15:28:27 +0200

    refactoring

---
+++ Assets/Scripts/Core/DebugUtils.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using System.Collections;
+
+public class DebugUtils : MonoBehaviour {
+
+// void OnDrawGizmosSelected()
+//     {
+//         if (_renderer && EDITOR_SHOW_BOUNDS)
+//         {
+//             Gizmos.color = new Color(0.2f, 0.2f, 0.2f, 0.5f);
+//             Gizmos.DrawWireCube(_renderer.bounds.center, _renderer.bounds.size);
+//         }        
+//     }
+
+// 	// Use this for initialization
+// 	void Start () {
+	
+// 	}
+	
+// 	// Update is called once per frame
+// 	void Update () {
+	
+// 	}
+}
